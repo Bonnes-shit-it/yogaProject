80d7b01af147e0bcc7423dcd5669312c2febf38b
Alexandr <casha_krasovski@mail.ru>
2020-02-25 10:46:49 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,55 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+
+    let deadline = '2020-02-24';
+
+    function getTime(endtime){
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60),
+            mins = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/1000/60/60));
+       
+        if(seconds < 10){
+               seconds= '0' + seconds;
+        }
+        if(hours < 10){
+            hours= '0' + hours;
+        }
+        if(mins < 10){
+            mins= '0' + mins;
+        }
+       
+        return {
+            'total' : t,
+            'seconds' : seconds,
+            'mins': mins,
+            'hours' : hours
+        }; 
+    }
+
+    function setClock(id,endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTime(endtime);
+
+            seconds.textContent = t.seconds;
+            hours.textContent = t.hours;
+            minutes.textContent = t.mins;
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+                seconds.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', deadline);
+
 });
